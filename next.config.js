962d0c00bb2bf5cd8b568dc5d9ccaba38bb46f79
Alanthadathil <alanthadathil73@gmail.com>
2023-08-02 15:28:29 +0530

    Next Todo App Completed

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    experimental: {
+        serverActions: true,
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
+// module.exports = {
+//     experimental: {
+//         serverActions: true,
+//     },
+// };
